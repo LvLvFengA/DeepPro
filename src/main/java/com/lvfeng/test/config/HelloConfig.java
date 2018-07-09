32c781c572cb0fd0e6a51a5cc157f542fb1bcf5a
LvLvFengA <LvLvFengA@github.com>
2018-07-09 17:01:13 +0800

    第一次提交测试：新增HelloConfig

---
+++ src/main/java/com/lvfeng/test/config/HelloConfig.java
@@ -0,0 +1,14 @@
+package com.lvfeng.test.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * java配置类，springboot4.0出现
+ * @author LvFeng
+ * 使用注解：@Configuration 表明了这是个控制类，控制类里一般存在@Bean注解
+ *
+ */
+@Configuration
+public class HelloConfig {
+	
+}
